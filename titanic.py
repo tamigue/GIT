1a4ff38d13820e9bb2cded8e829a73e650962431
tamigue <tamigue01@gmail.com>
2020-02-04 12:03:46 +0100

    Calcul du nombre de passager par catégorie et mise en tableau des données

--- titanic.py
+++ titanic.py
@@ -5,4 +5,26 @@ data = pd.read_excel("titanic3.xls")
 
 data = data.drop(['pclass', 'survived', 'name', 'sex', 'sibsp', 'parch', 'ticket',
        'fare', 'cabin', 'embarked', 'boat', 'body', 'home.dest'], axis=1)
-print(data)
+a = len(data)
+data = data.dropna(axis=0)
+b = len(data)
+c = a-b
+
+
+d = data[data["age"]<=20]
+d = len(d)
+
+
+e = data.loc[((data['age'] > 20) & (data['age'] <= 30))]
+e = len(e)
+
+f = data.loc[((data['age'] > 30) & (data['age'] <= 40))]
+f = len(f)
+
+g = data[data["age"]>40]
+g = len(g)
+
+h = c+d+e+f+g
+df1 = pd.Series(["cat0", "cat1", "cat2", "cat3","Personne sans age"])
+df = pd.DataFrame({"Nombre de personne":[d, e, f, g, c]}, index=df1)
+print(df)
